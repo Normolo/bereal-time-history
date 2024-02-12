55c3a28c4dcf26393423c39bf763b8085fe3bfc8
CosmologicalButter <53821637+Normolo@users.noreply.github.com>
2024-02-12 13:23:09 +0100

    Update ko-fi.js
    Minor optimisations

--- src/js/ko-fi.js
+++ src/js/ko-fi.js
@@ -5,23 +5,25 @@
 //	Created by Devin Baeten on 2023-03-03.
 //
 
+// Define the URL for the Ko-fi script
+const koFiScriptUrl = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+
 // Create a new script element
 const script = document.createElement('script');
 
 // Set the source URL for the script
-script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+script.src = koFiScriptUrl;
 
-// Add the script to the HTML <body> section
+// Add the script to the HTML <body> section to load the Ko-fi widget
 document.body.appendChild(script);
 
-// Wait for the script to finish loading
+// Once the script is loaded, draw the Ko-fi widget
 script.onload = function () {
-  // Draw the widget
   kofiWidgetOverlay.draw('devinbaeten', {
-	'type': 'floating-chat',
-	'floating-chat.donateButton.text': 'Support me',
-	'floating-chat.donateButton.background-color': '#d9534f',
-	'floating-chat.donateButton.text-color': '#fff'
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#d9534f',
+    'floating-chat.donateButton.text-color': '#fff'
   });
 };
 
